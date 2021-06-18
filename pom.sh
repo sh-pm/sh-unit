d7314aa6cce3dfde365032478ed8e407abbede47
sh-pm <shpmmail@gmail.com>
2021-06-18 07:51:35 -0300

    v1.5.5
    - Update sh-pm to v4.0.0

--- pom.sh
+++ pom.sh
@@ -1,15 +1,8 @@
 GROUP_ID=bash
 ARTIFACT_ID=sh-unit
-VERSION=v1.5.4
-
-declare -A REPOSITORY=( \
-	[host]="shpmcenter.com" \
-	[port]=443 \
-);
+VERSION=v1.5.5
 
 declare -A DEPENDENCIES=( \
-    [sh-pm]=v3.3.0
-    [sh-logger]=v1.4.0 \
+    [sh-pm]=v4.0.0@github.com/sh-pm \
+    [sh-logger]=v1.4.0@github.com/sh-pm \
 );
-
-SSO_API_AUTHENTICATION_URL=https://shpmcenter.com/sso/rest/api/sso/authentication
